7804b4aa5bdc74b705c56aeea4303abb4c23081c
LeeSangHwee <tkdgnl56@naver.com>
2024-08-12 20:45:52 +0900

    [level 1] Title: 모의고사, Time: 7.07 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,29 @@
+function solution(answers) {
+    var answer = [];
+    
+    let ScoreCount = [0, 0, 0]
+    let student = {
+        1:[1, 2, 3, 4, 5],
+        2:[2, 1, 2, 3, 2, 4, 2, 5],
+        3:[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]            
+    };
+ 
+    for(let index = 0; index < answers.length; index++)
+    {
+        for(let key in student)
+        {
+            if(student[key][index % student[key].length] == answers[index])
+                ScoreCount[key-1]++;
+        }
+    }
+
+    let maxScore = Math.max(...ScoreCount);
+
+    ScoreCount.forEach((HighScoreStdnt, index) => {
+        if (HighScoreStdnt === maxScore) {
+            answer.push(index + 1);
+        }
+    });
+    
+    return answer;
+}
