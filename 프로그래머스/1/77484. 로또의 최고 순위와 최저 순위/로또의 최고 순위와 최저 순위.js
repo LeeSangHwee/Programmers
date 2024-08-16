c0dbea83fcb9cef9fdb48b85acde3323ea9ce9d6
LeeSangHwee <tkdgnl56@naver.com>
2024-08-16 10:12:36 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,19 +1,9 @@
-function getRank(num) {
-    switch (num) {
-        case 6: return 1;
-        case 5: return 2;
-        case 4: return 3;
-        case 3: return 4;
-        case 2: return 5;
-        default: return 6;
-    }
-}
-
 function solution(lottos, win_nums) {           
+    var answer = [6, 6, 5, 4, 3, 2, 1];
+    
     const zero_count = lottos.filter(_lottos => 0 === _lottos).length;
     const min_count = win_nums.filter(_min_count=> lottos.includes(_min_count)).length
     const high_count = zero_count + min_count;    
-    
-    var answer = [getRank(high_count), getRank(min_count)];
-    return answer;
+        
+    return [answer[high_count], answer[min_count]]
 }
