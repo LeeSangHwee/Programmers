eac0b990edc6205bbe5a3a447bf36b8768b9f323
LeeSangHwee <tkdgnl56@naver.com>
2024-08-26 08:08:03 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,24 @@
+function solution(today, terms, privacies) {
+    var answer = [];
+    let termMonth = {};
+    const [todayYear, todayMonth, todayDay] = today.split(".").map(Number);
+    
+    terms.forEach(term => {
+        const [key, value] = term.split(" ");
+        termMonth[key] = Number(value);
+    });
+
+    privacies.forEach((customer_info, index) => {
+        const [date, key] = customer_info.split(" ");
+        let [dateYear, dateMonth, dateDay] = date.split(".").map(Number);
+        dateMonth += termMonth[key];
+        
+        while(dateMonth > 12) {
+            dateMonth -= 12;
+            dateYear++;            
+        }
+
+        if(todayYear > dateYear || (todayYear === dateYear && todayMonth > dateMonth) || (todayYear === dateYear && todayMonth === dateMonth && todayDay >= dateDay)) answer.push(index + 1);          
+    });
+    return answer;
+}
