25e6e7ac79b6ec16e77c5d7f0ab979f9e5bf2eb2
LeeSangHwee <tkdgnl56@naver.com>
2024-09-05 09:52:47 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,20 @@
+function GetGCD(a, b) {	
+	for (let GCD = a % b; GCD != 0; GCD = a % b) {		
+        a = b;
+		b = GCD;
+	}
+	return b;
+}
+
+function GetLCM(a, b, GCD) {
+    return a * b / GCD;
+}
+
+function solution(arr) {
+    var answer = 0;
+    answer = arr.reduce((acc, cur) => {
+        return GetLCM(acc, cur, GetGCD(acc, cur));
+    });
+    
+    return answer;
+}
