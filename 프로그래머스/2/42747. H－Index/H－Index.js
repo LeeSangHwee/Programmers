9b9b82cf0d70f60f8170e177493f8d335050951f
LeeSangHwee <tkdgnl56@naver.com>
2024-09-10 21:26:10 +0900

    [level 2] Title: H-Index, Time: 0.49 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,7 +1,9 @@
 function solution(citations) {
     citations.sort((a, b) => b - a);
     for(let index = 0; index < citations.length; index++) {       
-        if (citations[index] < index + 1)
+        // if (citations[index] >= index + 1)// 인용 횟수 >= 논문 순위, 인용 횟수가 논문 순위보다 크거나 같으면 true
+        // if (citations[index] <= index + 1)// 인용 횟수 <= 논문 순위, 인용 횟수가 논문 순위보다 작거나 같으면 true
+        if(citations[index] < index + 1)
             return index;
     }
     return citations.length;
