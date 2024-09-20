0c96362d3e2305c63cdbd72de9039c6a7e10548f
LeeSangHwee <tkdgnl56@naver.com>
2024-09-20 13:13:27 +0900

    [level 2] Title: 주차 요금 계산, Time: 28.58 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -2,7 +2,6 @@ function solution(fees, records) {
     var answer = [];
     let cars = {};
     
-    // fees [기본 시간, 기본 요금, 단위 시간, 단위 요금]
     records.forEach((carRecord) => {
         let [time, carNumber, status] = carRecord.split(" ");
         let [time_Hours, time_Minutes] = time.split(":").map(Number);
@@ -34,6 +33,5 @@ function solution(fees, records) {
     
     let sortCars = Object.keys(cars).sort();
     answer = sortCars.map(key => cars[key][1]);
-    
     return answer;
 }
